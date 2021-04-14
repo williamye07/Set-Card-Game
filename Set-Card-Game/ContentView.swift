2bb1160906d6fbc67254dec43e526b35600a8545
William Ye <williamye13@gmail.com>
2021-04-13 23:23:04 -0400

    Initial Commit

---
+++ Set-Card-Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set-Card-Game
+//
+//  Created by William Ye on 2021-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set-Card-Game/Set_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_Card_GameApp.swift
+//  Set-Card-Game
+//
+//  Created by William Ye on 2021-04-13.
+//
+
+import SwiftUI
+
+@main
+struct Set_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
